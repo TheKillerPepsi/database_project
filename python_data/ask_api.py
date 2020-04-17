79c4ca41cedede8104ba436663668099d19a323c
TheKillerPepsi <44766164+TheKillerPepsi@users.noreply.github.com>
2020-04-17 16:43:08 +0200

    Working on the Pythan database connection but ist just doesnt work...and i dunno why :(

--- python_data/ask_api.py
+++ python_data/ask_api.py
@@ -1,23 +1,31 @@
-#https://python.gotrained.com/python-json-api-tutorial/
 #requests to connect url
 #json to parse json
 import requests
 import json
-	
-url = "https://api.exchangeratesapi.io/latest"
-response = requests.get(url)
-data = response.text
+import mysql.connector
+
+##### API query ##########
+
+#url = "https://api.exchangeratesapi.io/latest"
+#response = requests.get(url)
+#data = response.text
 #print (data)
-parsed = json.loads(data)
+#parsed = json.loads(data)
 #print(json.dumps(parsed, indent=4))
-date = parsed["date"]
-print (date)
-usd_rates = parsed["rates"]["USD"]
-gbp_rate = parsed["rates"]["USD"]
-print (usd_rates)
-print("On "+ date +" EUR equals " + str(gbp_rate)+ " GBP")
-rates = parsed["rates"]
-	
-for currency, rate in rates.items():
-    print("EUR =",currency, rate)
+#date = parsed["date"]
+#print (date)
+#usd_rates = parsed["rates"]["USD"]
+#gbp_rate = parsed["rates"]["USD"]
+#print (usd_rates) 
+#print("On "+ date +" EUR equals " + str(gbp_rate)+ " GBP")
+#rates = parsed["rates"]	
+#for currency, rate in rates.items():
+#    print("EUR =",currency, rate)
  
+
+####### database work ######
+
+
+
+db = mysql.connector.connect (host = "sql", user = "root", passwd = "1234", db = "db.project")
+print(connection)
