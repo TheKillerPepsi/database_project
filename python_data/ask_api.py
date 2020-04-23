824873513a8dc90544f3c3924f17015a9cf38727
TheKillerPepsi <44766164+TheKillerPepsi@users.noreply.github.com>
2020-04-23 13:39:58 +0200

    Pythonscript implemented. If database is empty = insert. If not = Update. Next step is the php skript

--- python_data/ask_api.py
+++ python_data/ask_api.py
@@ -1,36 +1,46 @@
-#requests to connect url
-#json to parse json
 import requests
 import json
 import mysql.connector
 import time
-##### API query ##########
+import sys
 
-#url = "https://api.exchangeratesapi.io/latest"
-#response = requests.get(url)
-#data = response.text
-#print (data)
-#parsed = json.loads(data)
-#print(json.dumps(parsed, indent=4))
-#date = parsed["date"]
-#print (date)
-#usd_rates = parsed["rates"]["USD"]
-#gbp_rate = parsed["rates"]["USD"]
-#print (usd_rates) 
-#print("On "+ date +" EUR equals " + str(gbp_rate)+ " GBP")
-#rates = parsed["rates"]	
-#for currency, rate in rates.items():
-#    print("EUR =",currency, rate)
- 
+# establish api query
+url = "https://api.exchangeratesapi.io/latest"
+response = requests.get(url)
+data = response.text
+parsed = json.loads(data)
+rates = parsed["rates"]
 
-####### database work ######
-print("Deeebuuuuuug!!!!!!!!")
+# wait 10 seconds until the sql socket is up
+time.sleep(5)
 
-time.sleep(10)
-db = mysql.connector.connect (host = "sql", user = "root", passwd = "example", db = "db.project")
-print(db)
-#x = 0
-#while (db == False or x < 5):
- #   x += 1
-  #  db = mysql.connector.connect (host = "sql", user = "root", passwd = "1234", db = "db.project")
-   # print("Database connection wont work")
+# establish database connection
+db = mysql.connector.connect (host = "sql", user = "root", passwd = "1234", db = "db.project")
+mycursor = db.cursor()
+
+#check, if database is empty
+sql_check_exist = ("SELECT * FROM tb_devisen limit 1;")
+mycursor.execute(sql_check_exist)
+result = mycursor.fetchall()
+
+#if the table is empty, python enter the data
+if not result:
+    for (key, value) in rates.items():
+        add_data = ("INSERT INTO tb_devisen "
+              "(id, waehrung_iso, kurs) "
+                "VALUES (DEFAULT, %s, %s)")
+        key_value_data = (key, value)
+        mycursor.execute(add_data, key_value_data)
+        db.commit()
+    print("Added data")
+    #terminates the skript, so you dont need to update the data again
+    sys.exit()
+
+for (key, value) in rates.items():
+    insert_query = ("Update tb_devisen SET kurs = %s WHERE waehrung_iso = %s")
+    key_value_data = (value, key)
+    mycursor.execute(insert_query, key_value_data)
+    db.commit()
+print("Data updated")
+    #terminates the skript, so you dont need to update the data again
+#    sys.exit()
