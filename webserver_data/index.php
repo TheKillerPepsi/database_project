0b143e5b39ba2ccc1f4d131c0fb7ab2c27a8b22e
TheKillerPepsi <44766164+TheKillerPepsi@users.noreply.github.com>
2020-04-27 16:24:30 +0200

    build website with php. Just the data doing stuff is missing

--- webserver_data/index.php
+++ webserver_data/index.php
@@ -1,49 +1,71 @@
-<!DOCTYPE
-<HTML>
-  <HEAD>
-    <TITLE> Datenbankprojekt </TITLE>
-  </HEAD>
-  <BODY>
-   <?php
-  
-  //Hier wird die Verbindung erstellt. Das if-Statement prüft, ob die Vebrindung funktioniert hat
-$servername = "sql";
-$username = "root";
-$password = "1234";
-$database = "db.project";
-$conn = mysqli_connect($servername, $username, $password, $database );
-// Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
-  
-//hier setzen wir das sql-Statement ab (Von der Verbindung oben und dem defineirten Statement) und speichern das Ergebnis in einer Variable
-$sql = "SELECT * FROM tb_devisen;";
-$result = mysqli_query($conn, $sql ) or die("Error quering Database");
-  
-//Hier sagen wir, wenn er mehr Rows als null hat, soll er mit dem if-Statement weitermachen.
-if (mysqli_num_rows($result)>0)
-{ 
-  //Hier wird gesagt, solange SQL noch einen Array (mit der MEthode fetch_array) aus der Variablen result hat soll er das folgende machen. Ein Arrayteil ist damit eine Zelle einer Zeile
-  echo "<h1> Aktuelle Kurse: </h1>";
+<html> 
+    <head> 
+        <title>Dropdown</title> 
+    </head> 
+    <body> 
+       <h1> Willkommen auf meiner Website! </h1>
+       </br>
 
-  while($row = mysqli_fetch_array($result))
-  {
-      //Hier wird, aus der Row/Zeile sich der erste "Array" geschnappt (also Zelle) und dargestellt. Anschließend wird ein Zeilenumbruch gemacht
-      echo " ".$row[1]." ".$row[2];
-      echo "<br>";
-  }
+        <!-- Database connection -->
+        <?php
+                $mysqli = NEW MySQLi('sql', 'root', '1234', 'db.project');
+                
+                
+                
+        ?>
+        <form id="berechnung">
+        <!--  Dropdown_01 --->
+        <h3> Wähle die erste Währung aus: 
+        <select name="dropdown_01">
+        <?php
+            $result_dd_01 = $mysqli->query("SELECT waehrung_iso FROM tb_devisen;");
+            while($dd_01 = $result_dd_01->fetch_assoc())
+                {
+                    $name_dd_01 = $dd_01['waehrung_iso'];
+                    echo "<option value='$name_dd_01'>$name_dd_01</option>";
+                }
+        ?>   
+            </select>
+            </h3>
+            <br>
 
-  echo "<br>";
-  echo "<br>";
+        <!--  Dropdown_01 --->
+        <h3> Wähle die zweite Währung aus: 
+        <select name="dropdown_02">
+        <?php
+            $results_dd_02 = $mysqli->query("SELECT waehrung_iso FROM tb_devisen;");
+            while($dd_02 = $results_dd_02->fetch_assoc())
+                {
+                    $name_dd_02 = $dd_02['waehrung_iso'];
+                    echo "<option value='$name_dd_02'>$name_dd_02</option>";
+                }
+        ?>   
+            </select>
+            </h3>
+            <br>
 
- 
-}
+        <!-- Ausfüllfeld 1 --->
+        <h3> Bitte den Wert eingeben, den die erste Währung hat:
+        
+        <label for="input_data"> </label>
+        <input type="number" id=data name="data" min="o">
+        <input type="submit" value="Submit! :)">
+        <br>
+        </h3>
+        <br>
+        <!---Ergebnis ---->
+        
+        </form>
+        <!-- komplette Anzeige -->
+        <h2> Die aktuellen Kurse: </h2>
+        <?php
+        $results_every = $mysqli->query("SELECT * FROM tb_devisen;");
+        while($every = $results_every->fetch_assoc())
+        {
+            echo "<b> ".$every['waehrung_iso']."</b> ".$every['kurs'];
+            echo "<br>";
+        }
+        ?>
 
-mysqli_close($conn);
-
-  
-    ?>
-
-  </BODY>
-</HTML>
+    </body> 
+</html> 
