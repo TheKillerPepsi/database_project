04af23c433b083f05d42e60413db3a5db934e780
TheKillerPepsi <44766164+TheKillerPepsi@users.noreply.github.com>
2020-05-19 16:01:53 +0200

    Delete test.php

--- webserver_data/test.php
+++
@@ -1,5 +0,0 @@
-<form action="action.php" method="post">
- <p>Ihr Name: <input type="text" name="name" /></p>
- <p>Ihr Alter: <input type="text" name="alter" /></p>
- <p><input type="submit" /></p>
-</form>
