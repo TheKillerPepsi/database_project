ce415d77510739a28900a81b79ce8dc03312a4b6
TheKillerPepsi <44766164+TheKillerPepsi@users.noreply.github.com>
2020-06-19 10:19:07 +0200

    hat das HTML-Layout überarbeitet. Allerdings nur kosmetische Aspekte, nichts technisches

--- webserver_data/index.php
+++ webserver_data/index.php
@@ -3,10 +3,13 @@
 <html> 
     <head> 
         <meta charset="UTF-8">
-        <title>Database_Project</title> 
+        <title>Database_Project</title>
+        <link rel="stylesheet" href="styles.css">
     </head> 
-    <body> 
-       <h1> Moin Moin </h1>
+    <body>
+    <div id=wrapper>
+      
+        
        </br>
 
         <!-- Database connection -->
@@ -16,8 +19,77 @@
 
         <!-----Test Stuff -->
 
-        
+
+
+<div id="dropdowns">
+<h1> Moin Moin </h1>
+         <form action="" id="berechnung" method="post">
+        <!---------------------------  Dropdown_01 ------------------------------------>
+        <h3> Wähle die erste Währung aus:
+        <br> 
+        <select name="dropdown_01">
         <?php
+            $result_dd_01 = $mysqli->query("SELECT * FROM tb_devisen;");
+            while($dd_01 = $result_dd_01->fetch_assoc())
+                {   
+                    
+                     
+                    $iso_dd_01 = $dd_01['waehrung_iso'];
+                    echo "<option value=$iso_dd_01>$iso_dd_01</option>";
+                } 
+            echo  "</select>";
+            echo "</h3>";
+            echo "<br>";
+
+        ?> 
+        
+        <!---------------------------  Dropdown_02 -------------------------------------->
+        <h3> Wähle die zweite Währung aus:
+        <br> 
+        <select name="dropdown_02">
+        <?php
+            $results_dd_02 = $mysqli->query("SELECT * FROM tb_devisen;");
+            while($dd_02 = $results_dd_02->fetch_assoc())
+                {   
+                    
+                    $iso_dd_02 = $dd_02['waehrung_iso'];
+                    echo "<option value='$iso_dd_02'>$iso_dd_02</option>";
+                }
+        ?>   
+        </select>
+        </h3>
+        <br>
+
+        <!-- Ausfüllfeld 1 --->
+        <h3> Bitte den Wert eingeben, den die erste Währung hat:
+        <br>
+        <label for="input_data"> </label>
+        <input type="number" id=data name="data" min="o">
+        <input type="submit" value="Submit! :)" name="submit">
+        <br>
+        </h3>
+        <br>
+        </form>
+
+
+        </div>
+        <!---Ergebnis ---->
+        <div id="alle_kurse">
+        <?php
+        $result_all = $mysqli->query("SELECT * FROM tb_devisen;");
+        //Hier wird gesagt, solange SQL noch einen Array (mit der MEthode fetch_array) aus der Variablen result hat soll er das folgende machen. Ein Arrayteil ist damit eine Zelle einer Zeile
+        echo "<h1> Aktuelle Kurse: </h1>";
+        while($row = mysqli_fetch_array($result_all))
+        {
+            //Hier wird, aus der Row/Zeile sich der erste "Array" geschnappt (also Zelle) und dargestellt. Anschließend wird ein Zeilenumbruch gemacht
+            echo " ".$row[1].":   ".$row[2];
+             echo "<br>";
+        }
+        ?>
+        </div>
+        <div id="submit">
+        
+                <?php
             if(isset($_POST['submit']))
             {
             
@@ -51,72 +123,11 @@
           
         ?>
 
-                    <?php
-                       #build mysql stored procdure
-              
-
-
-?>
-         <form action="" id="berechnung" method="post">
-        <!---------------------------  Dropdown_01 ------------------------------------>
-        <h3> Wähle die erste Währung aus : 
-        <select name="dropdown_01">
-        <?php
-            $result_dd_01 = $mysqli->query("SELECT * FROM tb_devisen;");
-            while($dd_01 = $result_dd_01->fetch_assoc())
-                {   
-                    
-                     
-                    $iso_dd_01 = $dd_01['waehrung_iso'];
-                    echo "<option value=$iso_dd_01>$iso_dd_01</option>";
-                } 
-            echo  "</select>";
-            echo "</h3>";
-            echo "<br>";
-
-        ?> 
-        
-        <!---------------------------  Dropdown_02 -------------------------------------->
-        <h3> Wähle die zweite Währung aus: 
-        <select name="dropdown_02">
-        <?php
-            $results_dd_02 = $mysqli->query("SELECT * FROM tb_devisen;");
-            while($dd_02 = $results_dd_02->fetch_assoc())
-                {   
-                    
-                    $iso_dd_02 = $dd_02['waehrung_iso'];
-                    echo "<option value='$iso_dd_02'>$iso_dd_02</option>";
-                }
-        ?>   
-        </select>
-        </h3>
-        <br>
-
-        <!-- Ausfüllfeld 1 --->
-        <h3> Bitte den Wert eingeben, den die erste Währung hat:
-        
-        <label for="input_data"> </label>
-        <input type="number" id=data name="data" min="o">
-        <input type="submit" value="Submit! :)" name="submit">
-        <br>
-        </h3>
-        <br>
-        </form>
-        <!---Ergebnis ---->
-        <?php
-        $result_all = $mysqli->query("SELECT * FROM tb_devisen;");
-        //Hier wird gesagt, solange SQL noch einen Array (mit der MEthode fetch_array) aus der Variablen result hat soll er das folgende machen. Ein Arrayteil ist damit eine Zelle einer Zeile
-        echo "<h1> Aktuelle Kurse: </h1>";
-        while($row = mysqli_fetch_array($result_all))
-        {
-            //Hier wird, aus der Row/Zeile sich der erste "Array" geschnappt (also Zelle) und dargestellt. Anschließend wird ein Zeilenumbruch gemacht
-            echo " ".$row[1]." ".$row[2];
-             echo "<br>";
-        }
-        ?>
-}
+        </div>
 
 
 
+            </div>
+            </div>
     </body> 
 </html> 
